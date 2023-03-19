68343c109907d23db79df2e07034c065af7b5c21
deniz <denizderinalp@yahoo.com>
2023-03-19 13:58:44 +0000

    Added docstrings to climate\views.py

--- climate/views.py
+++ climate/views.py
@@ -7,25 +7,47 @@ from .models import Question, Choice
 
 
 def home(request):
+    """Function responds to website root HTTP requests
+
+        :returns: home.html template
+    """
     return render(request, 'climate/home.html')
 
 
 def about(request):
+    """Function responds to website root HTTP requests
+
+        :returns: about.html template
+    """
     return render(request, 'climate/about.html')
 
 
 def survey(request):
+    """Function responds to website root HTTP requests
+
+        :returns: survey.html template that shows the list of questions from database
+    """
     latest_question_list = Question.objects.order_by('-pub_date')
     context = {'latest_question_list': latest_question_list}
     return render(request, 'climate/survey.html', context)
 
 
 def detail(request, question_id):
+    """Function responds to website root HTTP requests
+
+        :param int question_id: id number and primary key of each question
+        :returns: detail.html template that shows the requested question and its options from database
+    """
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'climate/detail.html', {'question': question})
 
 
 def result(request, question_id):
+    """Function responds to website root HTTP requests
+
+       :param int question_id: id number and primary key of each question
+       :returns: result.html template that shows the result of the survey of voted question from database
+    """
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'climate/result.html', {'question': question})
 
