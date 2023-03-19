65c19a8db4d8c2e7f670957c34990cef4323fafb
deniz <denizderinalp@yahoo.com>
2023-03-19 23:16:55 +0000

    Allowed Host added to greenFuture/settings.py

--- greenFuture/settings.py
+++ greenFuture/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o-(sa@*rq^b67d58pl$gn-6-k)bjsyrl+9#wuycun1+k^yakj+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://ip172-18-0-18-cgbp5j8sf2q000eo2e20-8000.direct.labs.play-with-docker.com/']
 
 
 # Application definition
